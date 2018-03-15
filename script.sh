2e6a7dfeb0f00c0e253cad64ee691eafa320aa29
Denissk <denissk@metropolia.fi>
2018-03-15 05:29:57 +0200

    added gitignore and log old data

--- script.sh
+++ script.sh
@@ -6,5 +6,6 @@ outputFile="web/output.txt"
 wget -O $sourceFile https://steamdb.info/app/753/graphs/
 
 cat $sourceFile | grep -o '<li><strong>.*</li>' | sed 's/ <em.*em>\|<li><strong>\|<\/li>//g' | sed 's/<\/strong> /|/g'> $outputFile
+cp $outputFile oldData/output-`date +"%Y%m%d-%H%M"`.old
 
 rm $sourceFile
