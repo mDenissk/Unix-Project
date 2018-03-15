4cf7a3f20505b19c5f20c3ad6ba11942289473f7
Denissk <denissk@metropolia.fi>
2018-03-15 18:49:09 +0200

    few fixes script was fixed for crontab use now data is showed as table on the page

--- script.sh
+++ script.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
+directory="/home/den/Documents/Unix-Project/"
+cd $directory
+
 sourceFile="data.txt"
 outputFile="web/output.txt"
 
-wget -O $sourceFile https://steamdb.info/app/753/graphs/
+logDirectory="oldData/"
+outputFileLog=$logDirectory"output-`date +"%Y%m%d-%H%M"`.old"
+largeFileLog=$logDirectory"allStoredData.old"
 
-cat $sourceFile | grep -o '<li><strong>.*</li>' | sed 's/ <em.*em>\|<li><strong>\|<\/li>//g' | sed 's/<\/strong> /|/g'> $outputFile
-cp $outputFile oldData/output-`date +"%Y%m%d-%H%M"`.old
+wget -O $sourceFile https://steamdb.info/app/753/graphs/
+cat $sourceFile | grep -o '<li><strong>.*</li>' \
+				| sed 's/ <em.*em>\|<li><strong>\|<\/li>//g' \
+				| sed 's/<\/strong> /|/g'> $outputFile
+
+if [ ! -d "$logDirectory" ]
+then
+    mkdir $logDirectory
+fi
+
+cp $outputFile $outputFileLog
+
+if [ ! -f "$largeFileLog" ]
+then
+    touch $largeFileLog
+fi
+{ echo -n '<date>' ; date +"%Y-%m-%d-%H:%M" ; } >> $largeFileLog
+cat $outputFile >> $largeFileLog
 
 rm $sourceFile
